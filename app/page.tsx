de2eab9a4a6d9f33f68de17d29424458faa04d91
James Fang <jamesfangzr@gmail.com>
2023-12-12 01:14:31 -0600

    avatars for language flags

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,17 @@ export default function Home() {
 						src="/images/uiuc-snow-night.jpg"
 					/>
 					<p className = "text-center italic text-neutral-700">
-						Looking south from University of Illinois Urbana-Champaign (UIUC) in winter at night. From near to far, Illini Union, Main Quad, Foellinger Auditorium, South Quad, and Bell Tower. Isn't it pretty?
+						Welcome to my university. Looking south from University of Illinois Urbana-Champaign (UIUC) in winter at night. From near to far, Illini Union, Main Quad, Foellinger Auditorium, South Quad, and Bell Tower. Isn't it pretty?
 					</p>
 			</div>
 			
+			<div className="my-8">
+				<h1 className={title({ size: 'sm' })}>Current Work</h1>
+				<p className="my-4">
+					I am working on Bioimedge (pronounced bio-image), a zero- and low-code platform for computer vision for biomedical and healthcare purposes. 
+				</p>
+			</div>
+
 			<div className="my-8">
 				<h1 className={title({ size: 'sm' })}>Education</h1>
 				<ul className="list-disc list-inside indent-4 my-4">
@@ -49,7 +56,7 @@ export default function Home() {
 				</p>
 				<ul className="list-disc list-inside indent-4 my-4">
 					<li>Medical data science (i.e. preprocessing, mining)</li>
-					<li>General-purpose computer vision (architectures and optimizations)</li>
+					<li>General-purpose computer vision (i.e. architectures and training/validation optimizations for time & performance)</li>
 					<li>Algorithmic & mathematical theory (i.e. combinatorics)</li>
 					<li>Web/frontend development</li>
 					<li className = "indent-12">Still trying to get better at it, this personal page is my exercise!</li>
