68664ece7c2acfc5c98fc6b8424c3aac4b5289a5
James Fang <jamesfangzr@gmail.com>
2023-12-13 21:00:42 -0600

    hobbies update

--- app/hobbies/page.tsx
+++ app/hobbies/page.tsx
@@ -15,7 +15,7 @@ export default function HobbiesPage() {
 				<h1 className={title()}>What do I do for fun, other than nerdy CS stuff?</h1>
 				<h2 className={subtitle()}>Plenty of interesting things. Some nerdy as well.</h2>
 				<p className = "my-1">
-				I love playing piano, playing chess, cooking, working out, biking, hiking, travelling, and learning languages. Travelling and languages go hand-in-hand for me!
+				I love playing piano, playing chess, cooking, working out, biking, hiking, attending cultural events, travelling, and learning languages. Travelling and languages go hand-in-hand for me!
 				</p>
 				<br></br>
 				<p className = "my-1">
