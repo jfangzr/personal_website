9b7e238ac436aaef14f8110a339b77721abcae87
James Fang <jamesfangzr@gmail.com>
2023-12-13 20:02:20 -0600

    typo correction

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 		<div className="max-w-3xl mx-auto text-justify">
 			<div className="my-8">
 				<h1 className={title()}>Hi, I am James!</h1>
-				<h2 className={subtitle()}>I am a prospective Masters of Computer Science student at UIUC.</h2>
+				<h2 className={subtitle()}>I am a prospective Master of Computer Science student at UIUC.</h2>
 				
 			</div>
 
