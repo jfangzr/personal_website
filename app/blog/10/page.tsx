a22b2de39ea573978c7c512e4f2f89d9f48ef8cd
James Fang <jamesfangzr@gmail.com>
2024-02-05 02:35:24 -0600

    blog 8 edit and blog 10

---
+++ app/blog/10/page.tsx
@@ -0,0 +1,112 @@
+import { title, subtitle} from "@/components/primitives";
+import {Image, Link} from "@nextui-org/react";
+export default function ContactPage() {
+	return (
+		<div className="max-w-3xl mx-auto text-justify">
+			<div className = "my-4">
+				<Link href = "/blog/">Return to all blog posts</Link>
+			</div>
+			<div className="my-4">
+				<h1 className={title()}>10. How to find and listen to good advice</h1>
+				<h2 className={subtitle()}>How can someone separate the gold from the snake oil?</h2>
+				<p className = "my-4">
+					Again, this blog post will be updated with time. Everyday we all deal with different problems, and in my case, 
+					I am trying to figure out sales for my startup. There&apos;s a lot of advice out there about these, some of them 
+					designed to sell useless &quot;masterclasses&quot; that would leave me ten squares behind where I started. Finding 
+					people and resources that are genuinely trying to give me useful advice, such as <Link href="https://youtu.be/xZi4kTJG-LE?si=I6h5f4mg9rvmn-cO">How to 
+					Sell by Tyler Bosmeny</Link>, made me really think how I can get good, useful advice. 
+				</p>
+				<p className = "my-4 font-bold">
+					1. This someone reaffirms that your problem is difficult to solve 
+				</p>
+				<p className = "my-4">
+					Big questions, such as how to find customers as a company, are questions that are ageless. Those questions have already 
+					existed for a long time, from far before we were born.
+					Many, many people are asking for advice, spending a lot of money, and if an one-size-fits-all solution existed, everyone 
+					would be using it. Or rather, the people who have figured out the problem would all arrive at the same solution. 
+					If someone tells tries to tell me &quot;this one trick will solve all 
+					your problems related to [insert whatever you need fixed]&quot;, then it probably is not very good advice. I would be 
+					wary that the person is trying to sell me something that ultimately would not work. 
+				</p>
+				<p className = "my-4">
+					But if someone listens to me, explains the strengths and limitations of each option that I can go forward with,
+					then I know that someone is giving me something that I might need and adapting it to my problem case. At this point, 
+					the advice is still far from certain that it&apos;s good, but it is enough for me to give it a shot.  
+				</p>
+				<p className = "my-4 font-bold">
+					2. Someone should tell you what has <span className="italic">not</span> worked for them (or people they worked with)
+				</p>
+				<p className = "my-4">
+					This is someone trying to apply their wisdom to your problem case and telling you how to avoid the pitfalls 
+					they have sank through. That someone wants you to learn the fine details (the landscape) of the problem. This also immediately filters out a sign for someone to give you terrible advice: 
+					someone explaining what they did to <span className="italic">impress</span> you.  
+				</p>
+				<p className = "my-4">
+					In my opinion, people that are trying to impress you, whether it&apos;s with material possessions to show how successful they
+					are, or with knowledge (especially buzzword vomiting that falls apart under any serious logical examination). Those people 
+					are insecure and they do not want you to think about why their strategies probably will not work. What people that follow 
+					those awful advice would often find out is that they have only learned to put on fake façades, impressions, and hide their 
+					problems. At worst, they would delude themselves and not make substantial progress towards solving their problems on hand.    
+				</p>
+
+				<p className = "my-4">
+					Let&apos;s also say that I am much more willing to listen to someone who has unsubstantiable confidence in their claims 
+					rather than someone who is reasonable and think deeply into the strengths and limitations of the advice they give. This leads to...
+				</p>
+				<p className = "my-4 font-bold">
+					3. If someone tells you about the &quot;fine print&quot; and nuances of their advice, listen all in. 
+				</p>
+				<p className = "my-4">
+					If people giving advice to you are talking about the circumstances and If people giving you good advice want you to deeply think about their advice, that&apos;s a major sign that 
+					you are in good hands. They certainly don&apos;t want you to 
+					blindly follow it, and they want you to adapt the advice to best suit your needs. They don&apos;t want you 
+					to waste a ton of your time - remember, your time is valuable - and your money on something that they know
+					for near certainty will not work.  
+				</p>
+				<p className = "my-4">
+					Furthermore, if you feel like you can draw useful insights - such as principles of communication or a potential 
+					useful application to an adjacent problem - then I think it&apos;s a great sign that someone has given you at 
+					least <span className="italic">a little bit useful</span> advice. 
+				</p>
+				<p className = "my-4">
+					Which brings me to the last point...
+				</p>
+				<p className = "my-4 font-bold">
+					4. It is as much about the person as it is about the advice given. 	
+				</p>
+				<p className= "my-4 border-gray-500 border-t-2">
+				</p>
+				<p className = "my-4">
+					Lastly, I want to talk about perhaps the only silver bullet that solves your problems. And unfortunately, while 
+					I think it might work, it&apos;s not a &quot;get rich instantly&quot; solution. It&apos;s perhaps the exact opposite. 
+				</p>
+				<p className = "my-4">
+					And that&apos;s getting your solution process down to an exact science instead of an art. At the beginning, when, 
+					usually due to a lack of skill, solving problems feels like an art. We don&apos;t know how to approach it and 
+					try many things that don&apos;t work. As we learn more and get better at it, we learn more and more patterns, and 
+					center in on what works. We turn the problem-solving process from an art to a science for this specific problem. 
+				</p>
+				<p className = "my-4">
+					Getting the experience, however, there is no alternative to facing the problems head-on. Then it&apos;s about being 
+					patient to accumulate the experience necessary to form intuitions about approaching problems. This involves 
+					learning, thinking, and unfortunately in most cases, praying things would work out fine. Problems, while they 
+					may not have decreased in challenge level, hopefully become easier to solve when you&apos;re armed with your knowledge.   
+				</p>
+				<p className = "my-4">
+					Then hopefully your ingredients for success turn into a scientific process, controlled finely and carefully. For example, 
+					if startup success is a machine, then the cogs and gears of your machine might be:
+				</p>
+				<ul className="list-disc list-inside indent-4 my-4">
+					<li>Building a product users love</li>
+					<li>Selling your product to users</li>
+					<li>Hiring and managing the right people to work on the project</li>
+					<li>Fundraising</li>
+				</ul>
+				<p className = "my-4">
+					And you want to use your intuition gained from experience to think about how to maintain each cog/gear and think about how they all 
+					work together. 
+				</p>
+			</div>
+		</div>
+	);
+}
--- app/blog/8/page.tsx
+++ app/blog/8/page.tsx
@@ -34,6 +34,52 @@ export default function ContactPage() {
 					And that general mindset of coming up with ideas, thinking about &quot;what can I do differently&quot;, is something that 
 					made the prospect of starting a business so attractive to me. 
 				</p>
+				<p className= "my-4 border-black border-t-2">
+				</p>
+				<p className= "my-4">
+					Edit (Feb 5, 2024): I watched many videos from Y Combinator Startup School on YouTube (which, even though I haven&apos;t implemented 
+					all of their advice, I found them deeply insightful to apply). And I thought about even more reasons why 
+					I am excited to be working on my own startup.  
+				</p>
+				<p className= "my-4">
+					The first thing I thought about is how I react to frustration. In taking any career path, I inevitably will 
+					run into frustration somewhere. Doing something well is difficult. And working on my own projects, I realized that 
+					my response to frustration isn&apos;t to throw in the towel and thinking about quitting. Instead, my response is 
+					I&apos;ll probably complain a little bit, usually to a friend, to see if I can get some advice. I might take a break, 
+					by making a meal, go take a bike ride, or simply sleep. And then I realize that I would rather work on this than 
+					practically do anything else despite the challenges. 
+				</p>
+				<p className= "my-4">
+					When I was working on an internship with a manager that 
+					I had personality conflicts with, I was constantly dwelling on how I felt I was not treated the way that I wanted, 
+					how the friction in the project made me dislike the environment I was around - it was all around not the right fit
+					for me, if I want to put it nicely. I was only in it for a possible return offer that I didn&apos;t even really
+					want for the sake of possible future financial security. 
+				</p>
+				<p className= "my-4">
+					Working on PrepStar AI, however, I realized that I was actually interested in solving the problems I encountered. 
+					Yes, I absolutely wished that it was easy to get hundreds of thousands of students to use my product, and to 
+					convert them all into paying users. But I am interested in learning about how to solve these problems, and trying out 
+					solutions that result in varying levels of reach and success. 
+				</p>
+				<p className= "my-4">
+					Another thing that I realized that was I was working on problems that have real, meaningful impacts on 
+					my users. In this case, it is high school students who really want to do well on AP exams. I&apos;ve used 
+					my own product, and I actually felt impressed on many occasions on what my product could do. I&apos;ve heard 
+					about many people in big corporations working on projects that they don&apos;t actually want to do, or being 
+					shoeboxed into roles that they don&apos;t believe they want to take, even if the pay was great. 
+				</p>
+				<p className= "my-4">
+					However limiting startups can be in terms of work conditions - such as crazy work hours, unpredictability, and 
+					lower compensation, working at a startup is a philosophical and moral ownership of what you do. I can firmly say, 
+					&quot;I am working on my own product, for myself, making what I want to make&quot;. I am proud of 
+					the work that I have already done, the bits that I have launched, and I am excited to build more of the product 
+					to make my users love what I do even more.  
+				</p>
+				<p className= "my-4">
+					And I know that I will absolutely regret not trying more than the opportunities 
+					I <span className="italic">might</span> miss working on my startup. 
+				</p>
 			</div>
 		</div>
 	);
